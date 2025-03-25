f6333ef88a5dc79fc97feb25b5a58a5e90ea2ee4
Twostap <tyler.ostapyk@umanitoba.ca>
2025-03-25 09:20:21 -0500

    Update form.py

--- form.py
+++ form.py
@@ -16,9 +16,9 @@
 #################################################################################
 
 #Name: RxString
-#Description: This code produces a flask app for identifying drug data terms for knowledge synthesis projects
+#Description: This code produces a flask app that queries data sources for drug terms and creates a search string for use in bibliographic database searching
 #Author: Tyler Ostapyk (tyler.ostapyk@umanitoba.ca)
-#Date: March 19, 2025
+#Date: March 25, 2025
 
 # importing Flask and other modules
 from flask import Flask, request, render_template
