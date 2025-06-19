c7c7a01c6130404388e6947de0af075411fd48ad
Twostap <tyler.ostapyk@umanitoba.ca>
2025-06-19 14:33:21 -0500

    Update rxstring.py

--- rxstring.py
+++ rxstring.py
@@ -621,6 +621,7 @@ def drugdata():
                      dedupedstring = dedupedstring.replace(' CT ',' "CT" ')
                      dedupedstring = dedupedstring.replace(' RP ',' "RP" ')
                      dedupedstring = dedupedstring.replace(' use ',' "use" ')
+                     dedupedstring = dedupedstring.replace("/"," ")
                      FormattedPage = "<html><head><meta name='viewport' content='width=device-width, initial-scale=1.0'><style>h2 {font-size:1.4em;} .resultslogo {float:left;} .responsecontent {margin-left:120px; margin-top:25px;} @media only screen and (max-width: 600px) {.resultslogo {grid-area: 1 / span 3; float:none; height:auto;} .responsecontent {grid-area: 2 / span 3; margin-left:auto; margin-top:auto;} .resultsfooter {grid-area: 3 / span 3;} .resultslogoimage {display:block; margin-left:auto; margin-right: auto;}</style></head><body>" + "<div class='resultslogo' style='margin-top:10px'><img class='resultslogoimage' width='100px' src='https://raw.githubusercontent.com/Twostap/rxstring/refs/heads/main/rxstringcropped.png'/></div><div class='responsecontent' style='font-family:arial; font-size:.8em'><h2>Query Responses</h2>" + completeintro +  "<br><br>" +  "<form><input type='button' style='color: white; background-color: #01789f; border:1px solid black; padding:4px 10px' value='New search' onclick='window.history.go(-1)'></form>" + "<h2>Search string</h2>" + dedupedstring
  
              else:
